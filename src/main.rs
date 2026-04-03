3a0ae4ce1aadcc7626412ced343902c9d7a11fdd
Victor Palade <victor@cloudflavor.io>
2026-04-04 01:55:15 +0200

    chore: initialize seasons tauri workspace

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+mod app;
+mod hue;
+mod storage;
+mod ui;
+
+use app::*;
+use leptos::prelude::*;
+
+fn main() {
+    console_error_panic_hook::set_once();
+    mount_to_body(|| {
+        view! {
+            <App/>
+        }
+    })
+}
